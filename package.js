05ae783992a456e74b3b23a1f0562d6f8edd649f
Ben Lavalley <ben.lavalley@kaseya.com>
2019-10-26 23:47:35 -0400

    version bump

--- package.js
+++ package.js
@@ -1,11 +1,11 @@
 Package.describe({
   summary: "Simple cookies for browser.",
-  version: "1.1.0",
+  version: "1.1.1",
   git: "https://github.com/chuangbo/meteor-cookie.git"
 });
 
 Package.on_use(function (api) {
-  api.versionsFrom("METEOR@0.9.0");
+  api.versionsFrom("METEOR@1.8.1");
   api.export('Cookie', 'client');
   api.add_files('cookie.js', 'client');
 });
