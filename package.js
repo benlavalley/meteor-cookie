a7cfddc8c1e45dbf0f1462ee3c76a8159531de16
Ben Lavalley <ben.lavalley@kaseya.com>
2019-10-28 01:33:55 -0400

    version bump

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'chuangbo:cookie',
   summary: "Simple cookies for browser.",
-  version: "1.1.1",
+  version: "1.1.2",
   git: "https://github.com/chuangbo/meteor-cookie.git"
 });
 
